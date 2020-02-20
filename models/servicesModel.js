3b6e5aaf89d945d870f81a97687a15c65a0f64aa
sabbar iplex <sabbar.iplex@gmail.com>
2020-02-20 15:12:20 -0500

    upgrade services model

--- models/servicesModel.js
+++ models/servicesModel.js
@@ -2,16 +2,60 @@ const mongoose = require('mongoose');
 
 // create services schema
 const ServicesSchema = new mongoose.Schema({
-    name: {
+    test_name: {
         type: String,
         required: [true, "Name is required"]
     },
     price: {
-        type: String,
-        required: [true, "Price is required"]
+        type: String
+    },
+    code: {
+        type: String
+    },
+    department: {
+        type: String
+    },
+    completion_time: {
+        type: Number
+    },
+    tube_type: {
+        type: String
+    },
+    tube_color: {
+        type: String
+    },
+    sample_volume: {
+        type: String
+    },
+    test_description: {
+        type: String
     },
     params: {
-        type: []
+        type: [
+            {
+                params_name: {
+                    type: String
+                },
+                min_age: {
+                    type: Number
+                },
+                max_age: {
+                    type: Number
+                },
+                age_units: {
+                    type: String
+                },
+                gender: {
+                    type: String
+                },
+                ref_range: {
+                    type: String
+                },
+                sub_services: {
+                    type: String
+                },
+            }
+        ]
     }
 });
 
